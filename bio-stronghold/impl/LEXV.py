52ea83fbb83cb7691feac961de194007f5313725
Alexandra Mikhailova <mikhaylova.alexandra.a@gmail.com>
2017-10-13 12:32:30 +0100

    LEXV: Ordering Strings of Varying Length Lexicographically

--- bio-stronghold/impl/LEXV.py
+++ bio-stronghold/impl/LEXV.py
@@ -3,4 +3,31 @@ import solution
 
 class LEXV(solution.Solution):
 
-    pass
+    def _read(self, f):
+        alphabet = next(f).strip().split()
+        n = int(next(f).strip())
+        return alphabet, n
+
+    def _solve_helper(self, alphabet, l):
+        assert l > 0
+
+        if l == 1:
+            return alphabet
+
+        words = []
+
+        for a in alphabet:
+            # All words of length up to l starting with a.
+            words.append(a)
+            for w in self._solve_helper(alphabet, l - 1):
+                words.append(a + w)
+
+        return words
+
+    def _solve(self, data):
+        alphabet, n = data
+        return self._solve_helper(alphabet, n)
+
+    def _write(self, f, answer):
+        for word in answer:
+            f.write(word + '\n')
