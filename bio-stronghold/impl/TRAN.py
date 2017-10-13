218860da77b4cc71fc867158d2ebd87d6d9f55f5
Alexandra Mikhailova <mikhaylova.alexandra.a@gmail.com>
2017-10-13 13:07:02 +0100

    TRAN: Transitions and Transversions

---
+++ bio-stronghold/impl/TRAN.py
@@ -0,0 +1,25 @@
+import solution
+import utils
+
+
+class TRAN(solution.SimpleWriteSolution):
+
+    def _read(self, f):
+        return utils.read_fasta(f, dna_only=True)
+
+    def _solve(self, data):
+        transitions = 0
+        transversions = 0
+
+        s1, s2 = data
+
+        for c1, c2 in zip(s1, s2):
+            if c1 == c2:
+                continue
+
+            if (c1 in utils.PURINES) ^ (c2 in utils.PURINES):
+                transversions += 1
+            else:
+                transitions += 1
+
+        return transitions * 1. / transversions
