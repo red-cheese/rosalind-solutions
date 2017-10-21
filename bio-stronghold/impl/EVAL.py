e58c4b1aaa1e5a0fed2f4874d887843d00740d7e
Alexandra Mikhailova <mikhaylova.alexandra.a@gmail.com>
2017-10-21 20:56:55 +0100

    EVAL: Expected Number of Restriction Sites

---
+++ bio-stronghold/impl/EVAL.py
@@ -0,0 +1,21 @@
+import solution
+
+from impl import PROB
+
+
+_PROB = PROB.PROB()
+
+
+class EVAL(solution.ArrayWriteSolution):
+
+    def _read(self, f):
+        n = int(next(f).strip())
+        s = next(f).strip()
+        gcs = [float(gc) for gc in next(f).strip().split()]
+        return n, s, gcs
+
+    def solve(self, data):
+        n, s, gcs = data
+        probs_s = _PROB.solve((s, gcs), logprob=False)
+        answers = [(n - len(s) + 1) * p for p in probs_s]
+        return answers
