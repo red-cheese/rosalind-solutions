45ae2c507cd8a9bd817866a900f5456eb255bcbb
Alexandra Mikhailova <mikhaylova.alexandra.a@gmail.com>
2017-10-21 21:31:28 +0100

    MOTZ: Motzkin Numbers and RNA Secondary Structures

--- bio-stronghold/impl/CAT_MOTZ.py
+++ bio-stronghold/impl/CAT_MOTZ.py
@@ -12,13 +12,8 @@ class CAT(solution.SimpleWriteSolution):
         n = len(dna)
         assert n % 2 == 0
 
-        m = np.zeros(n * n, dtype=np.int64).reshape((n, n))
-
         # Starting values.
-        for i in range(n):
-            for j in range(n):
-                if i == j:
-                    m[i][j] = 1
+        m = np.eye(n, dtype=int)
 
         # Solve for all substrings of length l.
         for l in range(2, n + 2, 2):
@@ -53,3 +48,51 @@ class CAT(solution.SimpleWriteSolution):
             print()
 
         return m[0][-1]
+
+
+class MOTZ(solution.SimpleWriteSolution):
+
+    def _read(self, f):
+        return utils.read_fasta(f, dna_only=True)[0]
+
+    def solve(self, rna):
+        """Similarly to "Catalan" numbers above - solve for all substrings of
+        len l for l in range(n)."""
+
+        n = len(rna)
+        # Answers for substrings from i to j.
+        motz = np.eye(n, dtype=int)
+
+        for l in range(2, n + 1):
+            print('l = ', l)
+
+            for i in range(n - l + 1):
+                j = i + l - 1
+                assert i < j
+                # print('i = ', i, 'j = ', j)
+
+                # If rna[i] doesn't have an edge.
+                motz[i][j] += motz[i + 1][j]
+
+                # If rna[i] has an edge - possible edges ends on this step.
+                ends = [k for k in range(i + 1, j + 1)
+                        if rna[k] == utils.RNA_COMPLEMENTS[rna[i]]]
+                # print('ends: ', ends)
+
+                for end in ends:
+                    if end == j:
+                        motz[i][j] += 1 if i + 1 > j - 1 else motz[i + 1][j - 1]
+
+                    elif i + 1 == end:
+                        motz[i][j] += 1 if i + 2 > j else motz[i + 2][j]
+
+                    else:
+                        motz[i][j] += motz[i + 1][end - 1] * motz[end + 1][j]
+
+                    motz[i][j] %= 1000000
+
+                # print('i = ', i, 'j = ', j, 'm =', motz[i][j])
+
+            print()
+
+        return motz[0][-1]
